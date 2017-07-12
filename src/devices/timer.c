def99f665ed9faf13beb0f763e15d5d837b9a53e
kagong <dudrms5975@naver.com>
2017-07-12 10:09:03 +0000

    test commit

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -94,6 +94,7 @@ timer_sleep (int64_t ticks)
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
+  /*my_code*/
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
